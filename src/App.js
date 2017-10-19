1a018b47b94f8230b6437b5971be0fcf4a016b6a
Rehan Mallick <rmallick6806@gmail.com>
2017-10-19 11:36:55 -0700

    adding styles

--- src/App.js
+++ src/App.js
@@ -8,18 +8,22 @@ class App extends Component {
       <div className="what-card">
         <div className="what-card-header">
           <div className='what-card-name'>WhatCard</div>
+          <div className='flex-spacer'/>
+          <div className='what-card-nav'>Learn</div>
+          <div className='what-card-nav'>Select</div>
+          <div className='what-card-nav'>About</div>
         </div>
 
         <div className='outer-container'>
           <div className='row-card-container'>
             <div className="what-card-info card-type">
-              <h1>Step 1</h1>
+              <h1 className='merriweather-type'>Step 1</h1>
               <h1>Here we are putting in information about What Card</h1>
               <h4>Its really cool. Trust me. Here is more product information by the way. Here is more product information by the way. </h4>
             </div>
 
             <div className="what-card-action card-type">
-              <h1>Step 2</h1>
+              <h1 className='merriweather-type'>Step 2</h1>
               <h2>Here we are putting in information about What Card</h2>
               <h4>There will be a dropdown below this.</h4>
               <div className='card-button'>{`Get Started`}</div>
@@ -27,7 +31,7 @@ class App extends Component {
           </div>
 
           <div className="what-card-all card-type">
-            <h1>All Cards Available</h1>
+            <h1 className='merriweather-type'>How Credit Works</h1>
           </div>
         </div>
       </div>
